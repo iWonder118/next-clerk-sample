87e59f7d19ff95851e757563d45a3e7c8a053988
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2024-03-30 15:20:54 +0900

    modify buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,17 @@ export default async function Home() {
     <main className="flex flex-col justify-center items-center min-h-screen">
       トップページ
       <div className="flex justify-center items-center">
-        <Link href="/sign-in">サインイン</Link>
-        <Link href="/sign-up">サインアップ</Link>
+        <Link className="m-1 px-4 py-2 rounded-lg bg-blue-500 text-white text-sm" href="/sign-in">サインイン</Link>
+        <Link className="m-1 px-4 py-2 rounded-lg bg-secondary-500 text-gray-600 text-sm border border-black" href="/sign-up">サインアップ</Link>
+        <SignOutButton>
+          <button className="px-4 py-2 rounded-lg bg-red-500 text-white text-sm">
+            サインアウト
+          </button>
+        </SignOutButton>
       </div>
-      {}
-      <SignOutButton>
-        <button className="px-4 py-2 rounded-lg bg-blue-500 text-white text-sm">
-          サインアウト
-        </button>
-      </SignOutButton>
-      <UserInfo />
+      <div className="mt-2">
+        <UserInfo />
+      </div>
     </main>
   );
 }
